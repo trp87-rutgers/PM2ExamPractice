90400e975e44bf137d7044e555f693dffd1a1c62
trp87-rutgers <trp87@scarletmail.rutgers.edu>
2018-10-30 21:20:32 -0400

    not done with merge sort yet

---
+++ Mergesort.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Tim Petersen
+10/30/18
+Merge Sort
+Best Case: O(n)
+Average Case: O(n^2)
+Worst Case: O(n^2)
+Space Complexity: O(n)
+Stability: Yes
+"""
+
+
+def merge(arr, lo, med, hi):
+        
+    
+def sort():
+    
+    
+def main():
+    arr = [3, 5, 2, 7, 65, 10, 2, 1, 0]
+    
+    print(arr)
+    
+if __name__ == "__main__"
